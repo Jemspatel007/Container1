3e927d05b4ec967bfacddb423b3a6cf2f4a49988
jems <jm319109@dal.ca>
2024-07-12 17:17:39 -0300

    k8s-testing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 console.log("CICD Testing...")
 console.log("Testing");
 console.log("k8s Testing");
-
+console.log("Pods Updating testing");
 app.listen(6000, () => {
     console.log(`Listening on port ${6000}...!`);
 });
